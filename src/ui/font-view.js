f7dd492590c04b8be27d6d9a3165029d639fb2db
tol.is <me@tol.is>
2020-06-07 01:45:43 +0100

    font-view

--- src/ui/font-view.js
+++ src/ui/font-view.js
@@ -1,11 +1,15 @@
 import React, { useCallback, useMemo, useEffect, useState } from 'react';
 import propTypes from 'prop-types';
 import produce from 'immer';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { css } from 'emotion';
 import { otFeatures } from '../constants';
 
 const FontView = ({ id, index, metrics, config, setConfig, onRemove }) => {
+  const [showFeatures, setShowFeatures] = useState(false);
+  const [showVariations, setShowVariations] = useState(false);
+
   //
   const onRemoveClick = useCallback(() => {
     onRemove(id);
@@ -25,6 +29,36 @@ const FontView = ({ id, index, metrics, config, setConfig, onRemove }) => {
     },
     [config]
   );
+
+  const onLetterSpacingChange = useCallback((e) => {
+    const nextConfig = produce(config, (draft) => {
+      draft.letterSpacing = e.target.value;
+    });
+    setConfig(id, nextConfig);
+  });
+
+  const onFontSizeChange = useCallback((e) => {
+    const nextConfig = produce(config, (draft) => {
+      draft.fontSize = e.target.value;
+    });
+
+    setConfig(id, nextConfig);
+  });
+
+  const onLineHeightChange = useCallback((e) => {
+    const nextConfig = produce(config, (draft) => {
+      draft.lineHeight = e.target.value;
+    });
+    setConfig(id, nextConfig);
+  });
+
+  const onToggleFeaturesPanel = useCallback(() => {
+    setShowFeatures(!showFeatures);
+  }, [showFeatures]);
+
+  const onToggleVariationsPanel = useCallback(() => {
+    setShowVariations(!showVariations);
+  }, [showVariations]);
 
   //
   const fontFeatureSettings = useMemo(() => {
@@ -54,7 +88,7 @@ const FontView = ({ id, index, metrics, config, setConfig, onRemove }) => {
       <div
         className={css`
           display: grid;
-          grid-template-columns: repeat(5, minmax(0, 1fr));
+          grid-template-columns: repeat(10, minmax(0, 1fr));
           grid-gap: 1em;
         `}
       >
@@ -88,6 +122,26 @@ const FontView = ({ id, index, metrics, config, setConfig, onRemove }) => {
             min={8}
             max={400}
             step={1}
+            value={config.fontSize}
+            onChange={onFontSizeChange}
+          />
+        </div>
+        <div
+          className={css`
+            grid-column: span 1;
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <label htmlFor={`${id}_inputLineHeight`}>Line Height</label>
+          <input
+            id={`${id}_inputLineHeight`}
+            type="range"
+            min={0}
+            max={2}
+            step={0.01}
+            value={config.lineHeight}
+            onChange={onLineHeightChange}
           />
         </div>
         <div
@@ -101,41 +155,79 @@ const FontView = ({ id, index, metrics, config, setConfig, onRemove }) => {
           <input
             id={`${id}_inputLetterSpacing`}
             type="range"
-            min={-2}
-            max={2}
-            step={0.01}
+            min={-0.2}
+            max={0.2}
+            step={0.001}
+            value={config.letterSpacing}
+            onChange={onLetterSpacingChange}
           />
         </div>
         <div
           className={css`
-            grid-column: span 6;
+            grid-column: span 1;
+            display: flex;
+            flex-direction: column;
           `}
         >
-          <fieldset>
-            <legend>Open Type Features</legend>
-            <div
-              className={css`
-                display: grid;
-                grid-template-columns: repeat(5, minmax(0, 1fr));
-                grid-gap: 1em;
-                & > * {
-                  grid-column: span 1;
-                }
-              `}
-            >
-              {featureKeys.map((key) => (
-                <label>
-                  <input
-                    type="checkbox"
-                    name="features[]"
-                    value={key}
-                    onChange={onFontFeatureChange}
-                  />
-                  {otFeatures[key].title}
-                </label>
-              ))}
-            </div>
-          </fieldset>
+          <label>
+            <input
+              type="checkbox"
+              name="showFeatures"
+              checked={showFeatures}
+              onChange={onToggleFeaturesPanel}
+            />
+            Toggle Features
+          </label>
+        </div>
+        <div
+          className={css`
+            grid-column: span 1;
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <label>
+            <input
+              type="checkbox"
+              name="showVariations"
+              checked={showVariations}
+              onChange={onToggleVariationsPanel}
+            />
+            Toggle Variations
+          </label>
+        </div>
+        <div
+          className={css`
+            grid-column: span 10;
+          `}
+        >
+          <Accordion visible={showFeatures}>
+            <fieldset>
+              <div
+                className={css`
+                  display: grid;
+                  grid-template-columns: repeat(5, minmax(0, 1fr));
+                  grid-gap: 1em;
+                  & > * {
+                    grid-column: span 1;
+                  }
+                `}
+              >
+                {featureKeys.map((key) => (
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="features[]"
+                      value={key}
+                      checked={config.features[key]}
+                      onChange={onFontFeatureChange}
+                    />
+                    {otFeatures[key].title}
+                  </label>
+                ))}
+              </div>
+            </fieldset>
+          </Accordion>
         </div>
       </div>
       <div
@@ -145,12 +237,38 @@ const FontView = ({ id, index, metrics, config, setConfig, onRemove }) => {
           font-style: ${metrics.italic ? 'italic' : 'normal'};
           font-size: ${config.fontSize}px;
           line-height: ${config.lineHeight};
+          letter-spacing: ${config.letterSpacing}em;
           font-feature-settings: ${fontFeatureSettings};
+          padding: 2rem 0;
         `}
       >
         {config.text}
       </div>
     </div>
+  );
+};
+
+const Accordion = ({ visible, children }) => {
+  return (
+    <AnimatePresence initial={false}>
+      {visible && (
+        <motion.div
+          className={css`
+            overflow: hidden;
+          `}
+          initial="collapsed"
+          animate="open"
+          exit="collapsed"
+          variants={{
+            open: { opacity: 1, height: 'auto' },
+            collapsed: { opacity: 1, height: 0 },
+          }}
+          transition={{ type: 'spring', stiffness: 200, damping: 100 }}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
